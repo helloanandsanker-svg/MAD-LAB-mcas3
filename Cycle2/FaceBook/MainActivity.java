ffa155acc6e26920eaec3f7758aafe01dae60960
helloanandsanker <helloanandsanker@gmail.com>
2025-08-21 14:36:07 +0530

    Create MainActivity.java

---
+++ Cycle2/FaceBook/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.fbapp_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView facebookView=findViewById(R.id.fbView);
+        ImageView likeImgView=findViewById(R.id.likeView);
+        ImageView commentImgView=findViewById(R.id.cmmntView);
+        ImageView shareImgView=findViewById(R.id.shareView);
+
+        likeImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("You clicked Like button");
+            }
+        });
+
+        commentImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("You clicked Comment button");
+            }
+        });
+
+        shareImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("You clicked Share button");
+            }
+        });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
+}
